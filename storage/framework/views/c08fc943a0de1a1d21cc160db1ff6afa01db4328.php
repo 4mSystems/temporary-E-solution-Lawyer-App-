10b991371e1f822ee4099f720234935861737c69
DELL <mostafa_elnagar@2grand.net>
2020-07-20 06:25:52 +0200

    m

--- storage/framework/views/c08fc943a0de1a1d21cc160db1ff6afa01db4328.php
+++ storage/framework/views/c08fc943a0de1a1d21cc160db1ff6afa01db4328.php
@@ -33,9 +33,7 @@
 
 <div class="row">
     <div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-        <div class="logo">
-            <img src="<?php echo e(url('/images/logo.png')); ?>" style="width: 88px; height: 20px;">
-        </div>
+
         <!-- start: LOGIN BOX -->
         <div class="box-login">
             <h3 class="text-bold"><?php echo e(trans('site_lang.auth_cont_title')); ?></h3>
